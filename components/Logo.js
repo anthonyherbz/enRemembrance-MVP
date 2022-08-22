0b49cd785f9cf6a707436f2fa861429e59213cf0
jw <54646150+jwilborn-su@users.noreply.github.com>
2022-08-22 09:58:14 -0400

    Built text component

--- components/Logo.js
+++ components/Logo.js
@@ -1,9 +1,25 @@
 //Logo component to display SVG next to text in chosen font
 //reqs: Icon, link,
 //props: color, backgroundcolor, clickbehavior
-import Icon from '../components/Icon'
+import Icon from "../components/Icon";
+import Text from "../components/Text";
+
+function goHome(){
+	
+}
 
 const Logo = () => {
-	return <Icon  name = "logo_temp" color="blue" alt="enRemembrance icon" width="100" height="100"/>
-}
+	return (
+		<div onClick={goHome}>
+			<Icon
+				name='logo_temp'
+				color='blue'
+				alt='enRemembrance icon'
+				width='100'
+				height='100'
+			/>
+			<Text color="blue" size="3rem" fontWeight="bold" >enRemembrance</Text>
+		</div>
+	);
+};
 export default Logo;
--- components/Text.js
+++ components/Text.js
@@ -1,3 +1,23 @@
 //Probably use <span>
 //Will allow text to be bolded, italicized, super/subscript, color changed, etc
 //Props: bold, italic, superscript, subscript, color, highlight, size
+import styles from "./text.module.scss";
+import classNames from "classnames/bind";
+let cx = classNames.bind(styles);
+
+const Text = ({
+	children,
+	color = "black",
+	fontWeight = "normal",
+	size = "1rem",
+}) => {
+	let textClasses = cx({
+		text: true,
+		[`color-${color}`]: color,
+		[`font-weight-${fontWeight}`]: fontWeight,
+		[`size-${size}`]: size,
+	});
+
+	return <p className={textClasses}>{children}</p>;
+};
+export default Text;
