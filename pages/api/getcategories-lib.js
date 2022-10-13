f089c66b6a48ead7ec7cfa5fe3cd3fb3528f7806
jw <54646150+jwilborn-su@users.noreply.github.com>
2022-10-13 09:48:20 -0400

    Update getcategories-lib.js

--- pages/api/getcategories-lib.js
+++ pages/api/getcategories-lib.js
@@ -1,15 +1,14 @@
-import { query } from "../../lib/db"
+// import { query } from "../../lib/db"
 
-export default async function getServerSideProps(req, res) {
-	try {
-		const querySql = "SELECT * FROM categories"
-		const valuesParams = []
-		const data = await query({ query: querySql, values: valuesParams })
-		// (await db).end
-		const result = res.status(200).json({ categories: data })
-		return { props: result }
-	} catch (error) {
-		const result = res.status(400).json({ message: error.message })
-		return { props: result }
-	}
-}
+// export default async function getServerSideProps(req, res) {
+// 	try {
+// 		const querySql = "SELECT * FROM categories"
+// 		const valuesParams = []
+// 		const data = await query({ query: querySql, values: valuesParams })
+// 		const result = res.status(200).json({ categories: data })
+// 		return { props: result }
+// 	} catch (error) {
+// 		const result = res.status(400).json({ message: error.message })
+// 		return { props: result }
+// 	}
+// }
